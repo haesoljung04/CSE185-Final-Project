f9e5cba5e65ee223e81c941ae71e76429628e5b4
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-05-23 04:45:34 -0700

    Update myGWAS.py

--- myGWAS/myGWAS.py
+++ myGWAS/myGWAS.py
@@ -8,22 +8,22 @@ def main():
       description="Commmand line script to perform GWAS of given VCF and Phenotype Files"
   )
 
-# Required Arguments
-parser.add_argument('--vcf', type=str, required=True, help='Path to VCF file')
-parser.add_argument('--pheno', type=str, required=True, help='Path to phenotype file')
-
-# Optional arguments
-parser.add_argument('-o', '--out', type=str, help='Write output to File (Default: stdout)',\
-            required=False, metavar='FILE')
-parser.add_argument('--linear', action='store_true', help='Perform linear regression',\
-            required=False)
-parser.add_argument('--maf', type=float, default=0.05, help='Minor allele frequency threshold (default: 0.05)',\
-            required=False)
-parser.add_argument('--allow-no-sex', action='store_true', help='Allow samples without sex information',\
-            required=False)
-parser.add_argument('--summary', action='store_true', help='Print summary of statistics to a file')
-
-args = parser.parse_args()
+  # Required Arguments
+  parser.add_argument('--vcf', type=str, required=True, help='Path to VCF file')
+  parser.add_argument('--pheno', type=str, required=True, help='Path to phenotype file')
+  
+  # Optional arguments
+  parser.add_argument('-o', '--out', type=str, help='Write output to File (Default: stdout)',\
+              required=False, metavar='FILE')
+  parser.add_argument('--linear', action='store_true', help='Perform linear regression',\
+              required=False)
+  parser.add_argument('--maf', type=float, default=0.05, help='Minor allele frequency threshold (default: 0.05)',\
+              required=False)
+  parser.add_argument('--allow-no-sex', action='store_true', help='Allow samples without sex information',\
+              required=False)
+  parser.add_argument('--summary', action='store_true', help='Print summary of statistics to a file')
+  
+  args = parser.parse_args()
 
 if __name__ == "__main__":
     main()
