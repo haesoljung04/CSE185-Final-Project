790d76c042f6741cc556fa49ed4f68a6903143d1
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-05-24 02:13:17 -0700

    Update setup.py

--- tests/setup.py
+++ tests/setup.py
@@ -3,7 +3,7 @@ setup(
   version=VERSION,
   description='CSE185 Final Project',
   author='Haesol Jung, Meiqi Lai, Lydia Roh',
-  author_email='haj008@ucsd.edu, melai@ucsd.edu, lroh@ucsd.edu'
+  author_email='haj008@ucsd.edu, melai@ucsd.edu, lroh@ucsd.edu',
   packages=find_packages(),
   entry_points={
     "console_scripts": [
