10e92589002cf82b6de13cc3b72023edfd66523f
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-05-24 02:05:26 -0700

    Update setup.py

--- tests/setup.py
+++ tests/setup.py
@@ -1,1 +1,14 @@
-
+setup(
+  name='myGWAS',
+  version=VERSION,
+  description='CSE185 Final Project',
+  author='Haesol Jung, Meiqi Lai, Lydia Roh',
+  author_email='haj008@ucsd.edu, melai@ucsd.edu, lroh@ucsd.edu'
+  packages=find_packages(),
+  entry_points={
+    "console_scripts": [
+        "myGWAS=myGWAS.myGWAS:main"
+    ],
+  },
+)
+  
