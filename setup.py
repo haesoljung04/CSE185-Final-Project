59c83a95a15f70fd3b3c1bf65d8c6d8b49133826
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-05-24 02:31:14 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='myGWAS',
+  version='0.1.0',
+  description='CSE185 Final Project',
+  author='Haesol Jung, Meiqi Lai, Lydia Roh',
+  author_email='haj008@ucsd.edu, melai@ucsd.edu, lroh@ucsd.edu',
+  packages=find_packages(),
+  install_requires=[
+        "cyvcf2",
+        "scipy",
+        "pandas",
+        "seaborn",
+        "matplotlib",
+        "adjustText",
+        "setuptools", 
+  ],
+  entry_points={
+    "console_scripts": [
+        "myGWAS=myGWAS.myGWAS:main"
+    ],
+  },
+)
+  
