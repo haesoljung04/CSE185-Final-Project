a828105f8f9593ec32b0d774af77dd2dd7ec0c59
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-05-24 02:18:39 -0700

    Update setup.py

--- tests/setup.py
+++ tests/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='myGWAS',
-  version=VERSION,
+  version=0.1.0,
   description='CSE185 Final Project',
   author='Haesol Jung, Meiqi Lai, Lydia Roh',
   author_email='haj008@ucsd.edu, melai@ucsd.edu, lroh@ucsd.edu',
