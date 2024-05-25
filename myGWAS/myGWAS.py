6908209627aca06c7bcfd035839d9337a63f3b6e
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-05-24 18:53:31 -0700

    Update myGWAS.py

--- myGWAS/myGWAS.py
+++ myGWAS/myGWAS.py
@@ -72,12 +72,13 @@ def linear_regression(vcf_file, pheno_file, output_file, maf_threshold, allow_no
         # Calculate MAF to check if we include the variant in our calculations
         alleles = variant.gt_bases
         allele_counts = np.zeros(2, dtype=int)
-        for i, allele in enumerate(alleles):
-            allele_counts[i] = np.sum(variant.genotypes[:, i][variant.genotypes[:, i] != -1]) # Exclude missing genotypes (-1)
-        maf = np.min(allele_counts) / np.sum(allele_counts)
+        # implement this later
+        #for i, allele in enumerate(alleles):
+        #    allele_counts[i] = np.sum(variant.genotypes[:, i][variant.genotypes[:, i] != -1]) # Exclude missing genotypes (-1)
+        #maf = np.min(allele_counts) / np.sum(allele_counts)
         # If the maf is too small then do not include
-        if maf < maf_threshold:
-            continue
+        #if maf < maf_threshold:
+        #    continue
         
         # Prep data for linear regression
         genotype_data = []
