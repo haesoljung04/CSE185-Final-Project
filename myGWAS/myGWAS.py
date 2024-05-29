30c8f7692a354ec0d81bf11f56019556f5b39404
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-05-29 04:44:59 -0700

    Update myGWAS.py

--- myGWAS/myGWAS.py
+++ myGWAS/myGWAS.py
@@ -128,6 +128,7 @@ def linear_regression(vcf_file, pheno_file, output_file, maf_threshold, allow_no
     with Pool(cpu_count()) as p:
         with tqdm(total=num_variants, desc="Processing Variants") as pbar:
             for result in p.imap(lambda variant: process_variant(variant, samples, pheno_dict, index_dict, binary_mapping, maf_threshold), vcf):
+                print("for loop")
                 if result:
                     output.write("\t".join(map(str, result)) + "\n")
                     print("wrote a line")
