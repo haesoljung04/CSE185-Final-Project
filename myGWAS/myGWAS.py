c628d760d9d028cd424712ccbda08076a9caff3b
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-05-28 20:07:17 -0700

    Update myGWAS.py

--- myGWAS/myGWAS.py
+++ myGWAS/myGWAS.py
@@ -76,7 +76,9 @@ def linear_regression(vcf_file, pheno_file, output_file, maf_threshold, allow_no
     # Iterate through each variant using cyvcf2
     variantCount = 0
     for variant in vcf:
+        # Keep track of variant progress
         print(f"we are processing variant {variantCount}")
+        variantCount += 1
         # Calculate MAF to check if we include the variant in our calculations
         alleles = variant.gt_bases
         allele_counts = np.zeros(2, dtype=int)
